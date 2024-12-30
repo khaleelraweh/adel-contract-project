574d68af4ed01eced8063ba385cf94466d8ad5dd
khaleel raweh <khaleelvisa@gmail.com>
2024-12-30 16:32:41 +0300

    save all

--- resources/views/backend/contract_templates/index.blade.php
+++ resources/views/backend/contract_templates/index.blade.php
@@ -8,7 +8,7 @@
             <div class="card-naving">
                 <h3 class="font-weight-bold text-primary">
                     <i class="fa fa-folder"></i>
-                    {{ __('panel.manage_document_templates') }}
+                    {{ __('panel.manage_contract_templates') }}
                 </h3>
                 <ul class="breadcrumb pt-3">
                     <li>
@@ -20,13 +20,13 @@
                         @endif
                     </li>
                     <li class="ms-1">
-                        {{ __('panel.show_document_templates') }}
+                        {{ __('panel.show_contract_templates') }}
                     </li>
                 </ul>
             </div>
             <div class="ml-auto">
-                @ability('admin', 'create_document_templates')
-                    <a href="{{ route('admin.document_templates.create') }}" class="btn btn-primary">
+                @ability('admin', 'create_contract_templates')
+                    <a href="{{ route('admin.contract_templates.create') }}" class="btn btn-primary">
                         <span class="icon text-white-50 d-none d-sm-inline-block">
                             <i class="fa fa-plus-square">
 
@@ -38,7 +38,7 @@
             </div>
         </div>
 
-        @include('backend.document_templates.filter.filter')
+        @include('backend.contract_templates.filter.filter')
 
         <div class="card-body">
 
@@ -47,9 +47,7 @@
                 <thead>
                     <tr>
                         <th class="wd-5p border-bottom-0">#</th>
-                        <th class="wd-40p border-bottom-0">{{ __('panel.document_template_name') }}</th>
-                        <th class="wd-40p border-bottom-0">{{ __('panel.document_category_name') }}</th>
-                        <th class="wd-40p border-bottom-0">{{ __('panel.document_type_name') }}</th>
+                        <th class="wd-40p border-bottom-0">{{ __('panel.contract_template_name') }}</th>
                         <th class="wd-15p border-bottom-0 d-none d-sm-table-cell ">{{ __('panel.author') }}</th>
                         <th class="wd-15p border-bottom-0 d-none d-sm-table-cell ">{{ __('panel.status') }}</th>
                         <th class="wd-15p border-bottom-0 d-none d-sm-table-cell ">{{ __('panel.published_on') }}</th>
@@ -59,42 +57,36 @@
 
 
                 <tbody>
-                    @forelse ($documentTemplates as $document_template)
+                    @forelse ($contract_templates as $contract_template)
                         <tr>
                             <td class="text-center"><input type="checkbox" name="checkfilter"
-                                    value="{{ $document_template->id }}">
+                                    value="{{ $contract_template->id }}">
                             </td>
                             <td>
-                                {{ $document_template->doc_template_name }}
-                            </td>
-                            <td>
-                                {{ $document_template->documentType->documentCategory->doc_cat_name }}
-                            </td>
-                            <td>
-                                {{ $document_template->documentType->doc_type_name }}
+                                {{ $contract_template->doc_template_name }}
                             </td>
                             <td class="d-none d-sm-table-cell">
-                                {{ $document_template->created_by }}
+                                {{ $contract_template->created_by }}
                             </td>
                             <td class="d-none d-sm-table-cell">
-                                @if ($document_template->status == 1)
+                                @if ($contract_template->status == 1)
                                     <a href="javascript:void(0);" class="updateDocumentTemplateStatus "
-                                        id="document-template-{{ $document_template->id }}"
-                                        document_template_id="{{ $document_template->id }}">
+                                        id="document-template-{{ $contract_template->id }}"
+                                        contract_template_id="{{ $contract_template->id }}">
                                         <i class="fas fa-toggle-on fa-lg text-success" aria-hidden="true" status="Active"
                                             style="font-size: 1.6em"></i>
                                     </a>
                                 @else
                                     <a href="javascript:void(0);" class="updateDocumentTemplateStatus"
-                                        id="document-template-{{ $document_template->id }}"
-                                        document_template_id="{{ $document_template->id }}">
+                                        id="document-template-{{ $contract_template->id }}"
+                                        contract_template_id="{{ $contract_template->id }}">
                                         <i class="fas fa-toggle-off fa-lg text-warning" aria-hidden="true" status="Inactive"
                                             style="font-size: 1.6em"></i>
                                     </a>
                                 @endif
                             </td>
                             <td class="d-none d-sm-table-cell">
-                                {{ \Carbon\Carbon::parse($document_template->published_on)->diffForHumans() }}
+                                {{ \Carbon\Carbon::parse($contract_template->published_on)->diffForHumans() }}
                             </td>
                             <td>
                                 <div class="btn-group btn-group-sm">
@@ -112,19 +104,19 @@
                                         </a>
                                         <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
                                             <a class="dropdown-item d-flex align-items-center"
-                                                href="{{ route('admin.document_templates.edit', $document_template->id) }}">
+                                                href="{{ route('admin.contract_templates.edit', $contract_template->id) }}">
                                                 <i data-feather="edit-2" class="icon-sm me-2"></i>
                                                 <span class="">{{ __('panel.operation_edit') }}</span>
                                             </a>
 
-                                            {{-- @if ($document_template->documentPages->count() > 0) --}}
-                                            @if ($document_template)
+                                            {{-- @if ($contract_template->documentPages->count() > 0) --}}
+                                            @if ($contract_template)
                                                 <a href="javascript:void(0);"
                                                     class="dropdown-item d-flex align-items-center"
                                                     onclick="showAlert(
                                                         'warning', 
-                                                        '{{ __('panel.document_template_can_not_be_deleted') }}', 
-                                                        '{{ __('panel.document_template_have_pages_you_must_delete_pages_before') }}', 
+                                                        '{{ __('panel.contract_template_can_not_be_deleted') }}', 
+                                                        '{{ __('panel.contract_template_have_pages_you_must_delete_pages_before') }}', 
                                                         '{{ __('panel.ok') }}'
                                                     )">
                                                     <i data-feather="alert-circle" class="icon-sm me-2"></i>
@@ -133,14 +125,14 @@
                                             @else
                                                 <a href="javascript:void(0);"
                                                     class="dropdown-item d-flex align-items-center"
-                                                    onclick="confirmDelete('delete-document-template-{{ $document_template->id }}', '{{ __('panel.confirm_delete_message') }}', '{{ __('panel.yes_delete') }}', '{{ __('panel.cancel') }}')">
+                                                    onclick="confirmDelete('delete-document-template-{{ $contract_template->id }}', '{{ __('panel.confirm_delete_message') }}', '{{ __('panel.yes_delete') }}', '{{ __('panel.cancel') }}')">
                                                     <i data-feather="trash" class="icon-sm me-2"></i>
                                                     <span class="">{{ __('panel.operation_delete') }}</span>
                                                 </a>
                                                 <form
-                                                    action="{{ route('admin.document_templates.destroy', $document_template->id) }}"
+                                                    action="{{ route('admin.contract_templates.destroy', $contract_template->id) }}"
                                                     method="post" class="d-none"
-                                                    id="delete-document-template-{{ $document_template->id }}">
+                                                    id="delete-document-template-{{ $contract_template->id }}">
                                                     @csrf
                                                     @method('DELETE')
                                                 </form>
@@ -148,13 +140,13 @@
 
                                             <a href="javascript:void(0);"
                                                 class="dropdown-item d-flex align-items-center btn btn-success copyButton"
-                                                data-copy-text="https://ibbuniv.era-t.com/document_categories/{{ $document_template->slug }}"
-                                                data-id="{{ $document_template->id }}" title="Copy the link">
+                                                data-copy-text="https://ibbuniv.era-t.com/contract_categories/{{ $contract_template->slug }}"
+                                                data-id="{{ $contract_template->id }}" title="Copy the link">
                                                 <i data-feather="copy" class="icon-sm me-2"></i>
                                                 <span class="">{{ __('panel.operation_copy_link') }}</span>
                                             </a>
                                         </div>
-                                        <span class="copyMessage" data-id="{{ $document_template->id }}"
+                                        <span class="copyMessage" data-id="{{ $contract_template->id }}"
                                             style="display:none;">
                                             {{ __('panel.copied') }}
                                         </span>
