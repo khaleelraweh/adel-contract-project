527cbdd5c4b7b9d89feafdb4e2e8f7a42e36e6fd
khaleel raweh <khaleelvisa@gmail.com>
2024-12-30 16:03:27 +0300

    save all

--- database/seeders/ContractTemplateSeeder.php
+++ database/seeders/ContractTemplateSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\ContractTemplate;
+use Faker\Factory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +16,24 @@ class ContractTemplateSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = Factory::create();
+
+        ContractTemplate::create([
+            'name'              =>  ['ar'   =>  'عقد باسم العميل 1', 'en'  =>  'Contract named by Customer 1'],
+            'text'              =>  ['ar'   =>  $faker->text(), 'en'  =>  $faker->text()],
+            'file'              =>  '1.pdf',
+            'created_by'        => 'Admin System',
+            'status'            => true,
+            'published_on'      => $faker->dateTime(),
+        ]);
+
+        ContractTemplate::create([
+            'name'              =>  ['ar'   =>  'عقد باسم العميل 2', 'en'  =>  'Contract named by Customer 2'],
+            'text'              =>  ['ar'   =>  $faker->text(), 'en'  =>  $faker->text()],
+            'file'              =>  '2.pdf',
+            'created_by'        => 'Admin System',
+            'status'            => true,
+            'published_on'      => $faker->dateTime(),
+        ]);
     }
 }
