b91b54adf0c0fc4b4184286561ee843d8ff5ca53
khaleel raweh <khaleelvisa@gmail.com>
2024-12-30 16:37:17 +0300

    save all

--- public/backend/js/change-status.js
+++ public/backend/js/change-status.js
@@ -746,5 +746,29 @@ $(document).ready(function(){
         });
     });
 
+    //  updateContractTemplateStatus Status
+    $(document).on("click",".updateContractTemplateStatus",function(){
+        var status = $(this).children("i").attr("status");
+        var contract_template_id = $(this).attr("contract_template_id");
+
+        $.ajax({
+            headers:{
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
+            type:'post',
+            url:'/admin/contract-templates/update-contract-template-status',
+            data:{status:status,contract_template_id:contract_template_id},
+            success:function(resp){
+                if(resp['status']==0){
+                    $("#contract-template-"+contract_template_id).html("<i class='fas fa-toggle-off fa-lg text-warning' aria-hidden='true' status='Inactive' style='font-size:1.6em' />");
+                }else if (resp['status'] ==1 ){
+                    $("#contract-template-"+contract_template_id).html("<i class='fas fa-toggle-on fa-lg text-success' aria-hidden='true' status='Active' style='font-size:1.6em' />");
+                }
+            },error:function(){
+                alert("Error");
+            }
+        });
+    });
+
 
 });
