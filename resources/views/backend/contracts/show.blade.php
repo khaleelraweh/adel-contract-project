0d785cbdba2c5ea9be925577a5b9bbadff627bf7
khaleel raweh <khaleelvisa@gmail.com>
2025-01-13 23:00:39 +0300

    save all

--- resources/views/backend/contracts/show.blade.php
+++ resources/views/backend/contracts/show.blade.php
@@ -30,12 +30,12 @@
         <div class="my-auto">
             <div class="d-flex">
                 <h4 class="content-title mb-0 my-auto">
-                    <a href="{{ route('admin.documents.index') }}">{{ __('panel.manage_documents') }}</a>
+                    <a href="{{ route('admin.contracts.index') }}">{{ __('panel.manage_contracts') }}</a>
 
                 </h4>
                 <span class="text-muted mt-1 tx-13 mr-2 mb-0">
                     /
-                    {{ __('panel.show_documents') }}
+                    {{ __('panel.show_contracts') }}
                 </span>
             </div>
         </div>
@@ -44,7 +44,7 @@
 
             <div class="pr-1 mb-3 mb-xl-0">
                 {{-- <button type="button" class="btn btn-warning  btn-icon ml-2"><i class="mdi mdi-refresh"></i></button> --}}
-                <a href="{{ route('admin.document_templates.create') }}" class="btn btn-warning  btn-icon ml-2">
+                <a href="{{ route('admin.contract_templates.create') }}" class="btn btn-warning  btn-icon ml-2">
                     <i class="mdi mdi-refresh"></i>
                 </a>
             </div>
@@ -63,7 +63,7 @@
                     <div class="d-flex justify-content-between">
                         <h4 class="card-title mg-b-0">
                             <i class="fa fa-plus-square me-3 " style="font-size: 20px;"></i>
-                            {{ __('panel.show_documents') }}
+                            {{ __('panel.show_contracts') }}
                         </h4>
                         <i class="mdi mdi-dots-horizontal text-gray"></i>
                     </div>
@@ -72,46 +72,46 @@
                     <div class="table-responsive">
                         <table class="table">
                             <tr>
-                                <th>{{ __('panel.document_name') }}</th>
-                                <td>{{ $document->doc_name }}</td>
-                                <th>{{ __('panel.document_number') }}</th>
-                                <td>{{ $document->doc_no ?? '-' }}</td>
+                                <th>{{ __('panel.contract_name') }}</th>
+                                <td>{{ $contract->contract_name }}</td>
+                                <th>{{ __('panel.contract_number') }}</th>
+                                <td>{{ $contract->contract_no ?? '-' }}</td>
                             </tr>
                             <tr>
-                                <th>{{ __('panel.document_type') }}</th>
-                                <td>{{ $document->doc_type() }}</td>
+                                <th>{{ __('panel.contract_type') }}</th>
+                                <td>{{ $contract->contract_type() }}</td>
                                 <th>{{ __('panel.created_at') }}</th>
-                                <td>{{ $document->created_at }}</td>
+                                <td>{{ $contract->created_at }}</td>
                             </tr>
                             <tr>
-                                <th>{{ __('panel.document_status') }}</th>
-                                <td>{{ $document->doc_status() }}</td>
-                                <th>{{ __('panel.document_file') }}</th>
-                                <td>{{ $document->doc_file ?? '-' }} </td>
+                                <th>{{ __('panel.contract_status') }}</th>
+                                <td>{{ $contract->contract_status() }}</td>
+                                <th>{{ __('panel.contract_file') }}</th>
+                                <td>{{ $contract->contract_file ?? '-' }} </td>
                             </tr>
 
                         </table>
 
-                        <h3>{{ __('panel.document_text') }}</h3>
+                        <h3>{{ __('panel.contract_text') }}</h3>
                         <div class="card">
                             <div class="card-body">
-                                {!! $document->doc_content !!}
+                                {!! $contract->contract_content !!}
                             </div>
                         </div>
                     </div>
 
                     <div class="row">
                         <div class="col-12 text-center">
-                            <a href="{{ route('admin.documents.print', $document->id) }}"
+                            <a href="{{ route('admin.contracts.print', $contract->id) }}"
                                 class="btn btn-primary btn-sm ml-auto">
                                 <i class="fa fa-print"></i>
-                                {{ __('panel.document_print') }}
+                                {{ __('panel.contract_print') }}
                             </a>
 
-                            <a href="{{ route('admin.documents.pdf', $document->id) }}"
+                            <a href="{{ route('admin.contracts.pdf', $contract->id) }}"
                                 class="btn btn-secondary btn-sm ml-auto">
                                 <i class="fa fa-file-pdf"></i>
-                                {{ __('panel.document_export_pdf') }}
+                                {{ __('panel.contract_export_pdf') }}
                             </a>
 
                             {{-- <a href="{{ route('invoice.send_to_email', $invoice->id) }}"
