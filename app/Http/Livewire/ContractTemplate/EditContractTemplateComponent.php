26d669283dcdbc7eafe01eecb166105703af63c0
khaleel raweh <khaleelvisa@gmail.com>
2025-01-13 15:03:34 +0300

    save all

---
+++ app/Http/Livewire/ContractTemplate/EditContractTemplateComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\ContractTemplate;
+
+use Livewire\Component;
+
+class EditContractTemplateComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.contract-template.edit-contract-template-component');
+    }
+}
