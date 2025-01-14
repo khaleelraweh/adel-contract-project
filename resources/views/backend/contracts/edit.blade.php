e57b4ae833866ef32941d8f45b8653eee4574469
khaleel raweh <khaleelvisa@gmail.com>
2025-01-14 14:53:17 +0300

    save all

--- resources/views/backend/contracts/edit.blade.php
+++ resources/views/backend/contracts/edit.blade.php
@@ -43,7 +43,7 @@
         {{-- body part  --}}
         <div class="card-body">
             <div class="card-body">
-                @livewire('contracts.edit-contract-component', ['document_id' => $document->id])
+                @livewire('contracts.edit-contract-component', ['document_id' => $contract->id])
             </div>
         </div>
     @endsection
