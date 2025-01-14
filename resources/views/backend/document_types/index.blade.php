ee6eb81af4786bc407206efe511002603081bc87
khaleel raweh <khaleelvisa@gmail.com>
2025-01-14 18:51:33 +0300

    save all

--- resources/views/backend/document_types/index.blade.php
+++ resources/views/backend/document_types/index.blade.php
@@ -126,7 +126,7 @@
 
                                             <a href="javascript:void(0);"
                                                 class="dropdown-item d-flex align-items-center btn btn-success copyButton"
-                                                data-copy-text="https://ibbuniv.era-t.com/pages/{{ $document_type->slug }}"
+                                                data-copy-text="{{ config('app.url') }}/admin/document_types/{{ $document_type->id }}"
                                                 data-id="{{ $document_type->id }}" title="Copy the link">
                                                 <i data-feather="copy" class="icon-sm me-2"></i>
                                                 <span class="">{{ __('panel.operation_copy_link') }}</span>
