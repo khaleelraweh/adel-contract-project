8b4bc8c3c30823e9ebc78a1e09c0d004a92eb342
khaleel raweh <khaleelvisa@gmail.com>
2025-01-14 16:40:16 +0300

    save all

--- resources/views/backend/document_categories/show.blade.php
+++ resources/views/backend/document_categories/show.blade.php
@@ -5,11 +5,11 @@
             {{ __('Document Category Details') }}
         </div>
         <div class="card-body">
-            <h5>{{ $documentCategory->name }}</h5>
-            <p>{{ $documentCategory->description }}</p>
-            <p>Status: {{ $documentCategory->status ? __('Active') : __('Inactive') }}</p>
-            <p>Created at: {{ $documentCategory->created_at }}</p>
-            <p>Updated at: {{ $documentCategory->updated_at }}</p>
+            <h5>{{ $document_category->name }}</h5>
+            <p>{{ $document_category->description }}</p>
+            <p>Status: {{ $document_category->status ? __('Active') : __('Inactive') }}</p>
+            <p>Created at: {{ $document_category->created_at }}</p>
+            <p>Updated at: {{ $document_category->updated_at }}</p>
             <a href="{{ route('admin.document_categories.index') }}" class="btn btn-primary">
                 {{ __('Back to List') }}
             </a>
