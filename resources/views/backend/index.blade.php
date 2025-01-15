2e4201b8dcedb80de7ddca510507e5a5b856eac5
khaleel raweh <khaleelvisa@gmail.com>
2025-01-15 16:28:43 +0300

    save all

--- resources/views/backend/index.blade.php
+++ resources/views/backend/index.blade.php
@@ -6,7 +6,7 @@
             <h4 class="mb-3 mb-md-0">{{ __('panel.welcome_back') }}</h4>
         </div>
         <div class="d-flex align-items-center flex-wrap text-nowrap">
-            <div class="input-group flatpickr wd-200 me-2 mb-2 mb-md-0" id="dashboardDate">
+            {{-- <div class="input-group flatpickr wd-200 me-2 mb-2 mb-md-0" id="dashboardDate">
                 <span class="input-group-text input-group-addon bg-transparent border-primary" data-toggle><i
                         data-feather="calendar" class="text-primary"></i></span>
                 <input type="text" class="form-control bg-transparent border-primary" placeholder="Select date" data-input>
@@ -18,7 +18,7 @@
             <button type="button" class="btn btn-primary btn-icon-text mb-2 mb-md-0">
                 <i class="btn-icon-prepend" data-feather="download-cloud"></i>
                 Download Report
-            </button>
+            </button> --}}
         </div>
     </div>
 
